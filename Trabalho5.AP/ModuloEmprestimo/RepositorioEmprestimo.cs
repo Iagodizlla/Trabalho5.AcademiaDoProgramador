02889ec4fe7a363940eeec0d25682a632c178af8
Iagodizlla <iagoschlemper@gmail.com>
2025-04-09 21:23:27 -0300

    Criado modulos e suas classes iniciais

---
+++ Trabalho5.AP/ModuloEmprestimo/RepositorioEmprestimo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho5.AP.ModuloEmprestimo
+{
+    internal class RepositorioEmprestimo
+    {
+    }
+}
