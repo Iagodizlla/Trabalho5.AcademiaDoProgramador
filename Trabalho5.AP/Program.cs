cdcdc01e96de7c214ca523378e9a4b6f566238ed
Iagodizlla <iagoschlemper@gmail.com>
2025-04-09 21:08:36 -0300

    Adicionar arquivos de projeto.

---
+++ Trabalho5.AP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Trabalho5.AP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
